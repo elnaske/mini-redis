6e51a1b44bb6e0464d627cfff5b8ea98270978a7
elnaske <naske.elias@outlook.com>
2026-06-05 09:42:59 +0200

    Made client async to send concurrent requests

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,10 +1,9 @@
+use mini_redis::DEFAULT_ADDRESS;
 use mini_redis::server::Server;
-
-const ADDRESS: &str = "127.0.0.1:6379";
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let server = Server::new(ADDRESS).await?;
+    let server = Server::new(DEFAULT_ADDRESS).await?;
 
     server.run().await
 }
